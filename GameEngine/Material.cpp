94356e1ff04a1c4ea7829aad88cfad220f91034d
grahamstockton87 <grahamstockton6@gmail.com>
2025-06-29 20:39:34 -0700

    Revert "awadawd"
    This reverts commit 0245dfe6aa03c1696bea39d33d5375b46b83253c.

--- GameEngine/Material.cpp
+++ GameEngine/Material.cpp
@@ -18,21 +18,6 @@ void Material::UseMaterial(GLuint specularIntensityLocation, GLuint shininessLoc
 	glUniform1f(shininessLocation, shininess);
 }
 
-void Material::UseMaterial(GLuint specularMapLocation, GLuint usesSpecularMap, GLuint reflectivityLocation, GLuint skyboxLocation, GLuint usesReflectionsLocation)
-{
-	if (specularMap) {
-		glActiveTexture(GL_TEXTURE3);
-		glBindTexture(GL_TEXTURE_2D, specularMap->GetTextureID());
-		glUniform1i(specularMapLocation, 3);
-	}
-	glUniform1i(usesSpecularMap, hasSpecularMap);
-
-	glUniform1i(skyboxLocation, 5);
-	glUniform1i(reflectivityLocation, reflectivity);
-	glUniform1i(usesReflectionsLocation, usesReflections);
-}
-
-
 Material::~Material()
 {
 }
--- GameEngine/Material.h
+++ GameEngine/Material.h
@@ -2,7 +2,6 @@
 
 #include <GL/glew.h>
 #include "Texture.h"
-#include "Skybox.h"
 
 class Material {
 public:
@@ -10,20 +9,12 @@ public:
 	Material(GLfloat sIntensity, GLfloat shine);
 
 	void UseMaterial(GLuint specularIntensityLocation, GLuint shininessLocation);
-	void UseMaterial(GLuint specularMapLocation, GLuint usesSpecularMap, 
-		GLuint reflectivityLocation, GLuint skyboxLocation, GLuint usesReflections);
-	void SetSpecularMap(Texture* texture) { specularMap = texture; hasSpecularMap = (specularMap != nullptr); }
+
 	~Material();
 
 private:
 	GLfloat specularIntensity;
 	GLfloat shininess;
-
 	Texture* specularMap;
-	bool hasSpecularMap;
-
-	Skybox* skybox;
-	GLfloat reflectivity = 1.0f;
-	bool usesReflections = true;
 };
 
